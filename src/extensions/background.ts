237cfa1b4474cc2f4536fcb9432ffe6ac9feff84
LeeKyuSung <ks5050577@gmail.com>
2023-05-15 13:50:34 +0900

    read selected text & send content->background

--- src/extensions/background.ts
+++ src/extensions/background.ts
@@ -1,1 +1,8 @@
 console.log("background script loaded");
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.hasOwnProperty("kyusung") && request.kyusung === "kyusung") {
+    console.log(request.text);
+    sendResponse({});
+  }
+});
