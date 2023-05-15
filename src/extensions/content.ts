5eeea206055442eb25b6c868ee69508bf6b4204b
LeeKyuSung <ks5050577@gmail.com>
2023-05-15 23:31:25 +0900

    프로젝트 다시 세팅
    프로젝트 다시 세팅

--- src/extensions/content.ts
+++
@@ -1,9 +0,0 @@
-console.log("contents test script");
-
-document.onmouseup = function () {
-  const selectedText = window.getSelection()?.toString() || "";
-  console.log(selectedText);
-  if (selectedText.length > 0) {
-    chrome.runtime.sendMessage({ text: selectedText, kyusung: "kyusung" });
-  }
-};
