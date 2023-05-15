5cce370d7149e55b154e26d2e0a3243e973b14ee
LeeKyuSung <ks5050577@gmail.com>
2023-05-15 22:44:28 +0900

    add selected test on popup

--- src/extensions/background.ts
+++ src/extensions/background.ts
@@ -4,5 +4,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.hasOwnProperty("kyusung") && request.kyusung === "kyusung") {
     console.log(request.text);
     sendResponse({});
+    chrome.runtime.sendMessage({
+      kyusung: "kyusung",
+      text: request.text,
+    });
   }
 });
